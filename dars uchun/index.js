260b26b2d24510a5d050a03cb5016c429bbb0aa9
samandargofurov <samandargofurov236@gmail.com>
2023-12-16 22:35:23 +0500

    5 lesson

---
+++ dars uchun/index.js
@@ -0,0 +1,66 @@
+// let n = 25;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     if (n % i == 0){
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
+
+
+
+// let n = 3;
+// let sum = 0;
+// let counter = 0;
+
+// for (let i = 2; i <= n; i++) {
+//     counter = 0;
+
+//      for (let j = 1; j <= i; j++) {
+//         if (i % j == 0) {
+//             counter++;
+//         }
+//      }
+
+//      if (counter == 2){
+//         sum += i;
+//      }
+// }
+
+// console.log(sum);
+
+
+// let n = 5;
+// let sum = 1;
+// let i = 1;
+// while (i <= n) {
+//     if (i % 2 == 0){
+//         sum *= i;
+//     }
+//     i++;
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+// let n = 1344;
+// let i = 1;
+// let sum = 0;
+
+// while(i <= n){
+    
+//     i++;
+
+//     console.log(sum);
+// }
---
+++ index.js
@@ -0,0 +1,84 @@
+// 1-masala
+// a dan b gacha bo'lgan sonlar ko'paytmasini topuvchi algoritm yozing?
+
+// let a = 5;
+// let b = 10;
+// let sum = 1;
+
+// while (a <= b) {
+//     sum *= i;
+// }
+
+// console.log(sum);
+
+
+// 2-masala
+// Berilgan sonning necha xonali ekanligini aniqlovchi algoritm yozing?
+
+// let n = 1234;
+// let counter = 0;
+
+// for(let i = 1; i <= n; i++){
+//         i = n % 10;
+//         i = Math.floor(n / 10) % 10;
+//         i = Math.floor(n / 100) % 10;
+//         i = Math.floor(n / 1000);
+        
+//         counter++;
+//     }
+
+// console.log(res, counter);
+
+// 3-masala
+// Berilgan sonning faktorialini topuvchi algoritm yozing?
+
+// let n = 5;
+// let counter = 0;
+// let sum = 1;
+
+// for(let i = 1; i <= n; i++){
+//     sum *= i;
+//     counter++;
+// }
+
+// console.log(counter);
+
+// 4-masala
+// Foydalanuvchidan parol kiritishini so'raydigan dastur yozing.
+// Foydalanuvchi to'g'ri parolni kiritmaguncha so'rashni davom ettirish uchun WHILE tsiklidan foydalaning.
+
+
+// const togriParol = "salom";
+
+// let n = prompt("Parolni kiriting:");
+
+// while (n !== togriParol) {
+//     n = prompt("parolni qaytadan kiriting:");
+// }
+
+// console.log("Parol to'g'ri kiritildi:");
+
+// 5-masala
+// Birinchi n ta tub sonning yig'indisini while tsiklidan foydalanib hisoblaydigan dastur yozing.
+
+
+
+
+
+// 6-masala
+// Berilgan sonni teskari tartibda yozuvchi algoritm yozing.
+
+let n = 123;
+let res;
+let counter = 0;
+
+let i = 1;
+while (1 <= n) {
+    res = n / 100;
+    res = (n / 10) % 10;
+    res = (n % 10);
+
+    counter++;
+}
+
+console.log(res);
